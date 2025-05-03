cc6c3fbcf149f27232d5222739cfab08339b1c48
Venom0807 <khanahmer420@gmail.com>
2025-05-03 17:38:51 +0500

    fuckkk

--- src/Components/Common/Poster.jsx
+++ src/Components/Common/Poster.jsx
@@ -5,7 +5,7 @@ const Poster = () => {
     <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto">
       <div class="lg:w-full mx-auto ">
-        <div class="poster flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
+        <div class="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4 poster">
           <div class="text-center text-white relative z-10 w-full">
             <h2 class="text-4xl font-bold title-font mb-2">GET UPDATE FROM ANYWHERE</h2>
             <p class="leading-relaxed">Bearing Void gathering light light his eavening unto dont afraid.</p>

eb42b4ff721d747b60509c5ed4d306f64b87b783
Venom0807 <khanahmer420@gmail.com>
2025-05-03 16:53:45 +0500

    singlePage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Common/Hero'
-import AHero from '../Components/Common/aHero'
+import AHero from '../Components/Common/AHero'
 import ProjectCards from '../Components/Common/ProjectCards'
 import Poster from '../Components/Common/poster'
 

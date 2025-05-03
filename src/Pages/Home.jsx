67ba4572bccc0c1e610a199a7b9c72a2fcc6b025
Venom0807 <khanahmer420@gmail.com>
2025-05-03 16:57:25 +0500

    SinglePage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../Components/Common/Hero'
 import AHero from '../Components/Common/AHero'
 import ProjectCards from '../Components/Common/ProjectCards'
-import Poster from '../Components/Common/poster'
+import Poster from '../Components/Common/Poster'
 
 const Home = () => {
   return (

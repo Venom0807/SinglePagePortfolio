d89bf9a72f8cca9285d2ba6fa3181241ce9cbfdf
Venom0807 <khanahmer420@gmail.com>
2025-05-11 13:08:23 +0500

    Update message

--- src/Components/Common/ProjectCards.jsx
+++ src/Components/Common/ProjectCards.jsx
@@ -7,7 +7,7 @@ const ProjectCards = () => {
       <h1 className='text-4xl text-black font-bold my-10'>QUALITY WORK<br />RECENTLY DONE PROJECTS</h1>
     <div class="flex flex-wrap -m-4">
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden cursor-pointer">
+        <a class="block relative h-48 rounded overflow-hidden cursor-pointer" href='https://academia-seven-omega.vercel.app/'>
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="p1.png"/>
         </a>
         <div class="mt-4">
@@ -16,7 +16,7 @@ const ProjectCards = () => {
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden cursor-pointer">
+        <a class="block relative h-48 rounded overflow-hidden cursor-pointer" href='https://acme-drab-three.vercel.app/'>
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="p2.png"/>
         </a>
         <div class="mt-4">
@@ -25,7 +25,7 @@ const ProjectCards = () => {
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden cursor-pointer">
+        <a class="block relative h-48 rounded overflow-hidden cursor-pointer" href='https://code-upscale.vercel.app/'>
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="p3.png"/>
         </a>
         <div class="mt-4">
@@ -34,7 +34,7 @@ const ProjectCards = () => {
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden cursor-pointer">
+        <a class="block relative h-48 rounded overflow-hidden cursor-pointer" href='https://e-store-gules-six.vercel.app/'>
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="p4.png"/>
         </a>
         <div class="mt-4">
@@ -43,7 +43,7 @@ const ProjectCards = () => {
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden cursor-pointer">
+        <a class="block relative h-48 rounded overflow-hidden cursor-pointer" href='https://genial-one.vercel.app/'>
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="p5.png"/>
         </a>
         <div class="mt-4">
@@ -52,7 +52,7 @@ const ProjectCards = () => {
         </div>
       </div>
       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden cursor-pointer">
+        <a class="block relative h-48 rounded overflow-hidden cursor-pointer" href='https://neve-zeta.vercel.app/'>
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="p6.png"/>
         </a>
         <div class="mt-4">
